ac8d624309d23742ee8bfe0d1c510b87dcb0683a
Owade <82964062+Nelwade@users.noreply.github.com>
2022-06-12 20:53:55 +0300

    This will add requirements file and changed database sqlite

--- limehome/settings.py
+++ limehome/settings.py
@@ -80,13 +80,21 @@ WSGI_APPLICATION = 'limehome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'limehome',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'limehome',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + '/' + 'db.sqlite3',
     }
 }
 
